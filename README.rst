78af6d431e56ff8f9beab314143d77613d166c36
Ross Jones <ross@servercode.co.uk>
2015-03-18 20:17:59 +0000

    Initial commit

---
+++ README.rst
@@ -0,0 +1,34 @@
+=============
+ckanext-officedocs
+=============
+
+This plugin provides the option of using the Microsoft Office viewer for
+previewing content as an IResourceView
+
+
+------------
+Installation
+------------
+
+To install ckanext-officedocs:
+
+1. Clone this repository into the place where you normally install extensions,
+   by default this will be /usr/lib/ckan/default/src/
+
+2. Activate your CKAN virtual environment, for example::
+
+     . /usr/lib/ckan/default/bin/activate
+
+3. Install the ckanext-gdoc Python package into your virtual environment::
+
+     cd ckanext-officedocs
+     python setup.py install
+
+3. Add ``officedocs`` to the ``ckan.plugins`` setting in your CKAN
+   config file (by default the config file is located at
+   ``/etc/ckan/default/production.ini``).
+
+4. Restart CKAN. For example if you've deployed CKAN with Apache on Ubuntu::
+
+     sudo service apache2 reload
+
---
+++ ckanext/officedocs/plugin.py
@@ -0,0 +1,38 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+class OfficeDocsPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IResourceView)
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'officedocs')
+
+    def info(self):
+        return {
+            "name": "officedocs_view",
+            "title": toolkit._('Microsoft Office Previewer'),
+            "default_title": toolkit._('Preview'),
+            "icon": "compass",
+            "always_available": True,
+            "iframed": False,
+        }
+
+    def setup_template_variables(self, context, data_dict):
+        from urllib import quote_plus
+        url = quote_plus(data_dict["resource"]["url"])
+        return {
+            "resource_url": url
+        }
+
+    def can_view(self, data_dict):
+        return data_dict['resource']['format'].lower() in ['doc', 'pdf', 'xls', 'xlsx']
+
+    def view_template(self, context, data_dict):
+        return "officedocs/preview.html"
+
+    def form_template(self, context, data_dict):
+        return "officedocs/form.html"
